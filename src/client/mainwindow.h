45bfae62cd7fbcd6fd0a8fb393c5c10803a5ed21
DanceRain <1477364283@qq.com>
2021-01-13 21:30:09 +0800

    project update

---
+++ src/client/mainwindow.h
@@ -0,0 +1,33 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMouseEvent>
+#include <QPushButton>
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+protected:
+    void mousePressEvent(QMouseEvent* e);
+    void mouseMoveEvent(QMouseEvent* e);
+    void mouseReleaseEvent(QMouseEvent* e);
+
+private slots:
+    void closeWindow();
+
+private:
+    void init();
+    Ui::MainWindow *ui;
+    QPoint last;
+};
+
+#endif // MAINWINDOW_H
